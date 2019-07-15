e4ecd0e27b4308b4cd0c66824eb1796bc03e2420
yo_artyom <q3pp@yandex.ru>
2019-07-15 11:20:27 +0200

    tmux conf

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 rm -f ~/.vimrc
 rm -f ~/.ackrc
 rm -f ~/.zshrc
+rm -f ~/.tmux.conf
 rm -rf ~/.config/nvim
 
 ln -s `pwd`/.vimrc  ~/.vimrc
@@ -10,5 +11,6 @@ ln -s `pwd`/nvim ~/.config/nvim
 
 ln -s `pwd`/.zshrc ~/.zshrc
 ln -s `pwd`/.ackrc ~/.ackrc
+ln -s `pwd`/.tmux.conf ~/.tmux.conf
 
 echo 'Done c:'
