b3060c13aaf7dd76485d885376261d18463309d1
amalyshev <q3pp@yandex.ru>
2020-02-20 12:19:48 +0100

    Added brewfile, update install script

---
+++ Brewfile
@@ -0,0 +1,8 @@
+brew "ack"
+brew "bat"
+brew "jq"
+brew "neovim"
+brew "rbenv"
+brew "tmux"
+brew "zsh"
+brew "zsh-completions"
--- install.sh
+++ install.sh
@@ -1,30 +1,20 @@
 #!/bin/bash
 
-read -p "Do you want to remove old config files? [y/n]" -n 1 -r
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-  rm -f ~/.ackrc
-  rm -f ~/.alacritty.yml
-  rm -f ~/.global_gitignore
-  rm -f ~/.tmux.conf
-  rm -f ~/.zshrc
-
-  rm -f ~/.vimrc
-  rm -rf ~/.config/nvim
-fi
-
+echo 'Creating symlinks to configs...'
 
 # nvim config copy
-ln -s `pwd`/nvim/.vimrc  ~/.vimrc
-ln -s `pwd`/nvim ~/.config/nvim
+ln -sf `pwd`/nvim/.vimrc  ~/.vimrc
+ln -sf `pwd`/nvim ~/.config/nvim
 
-ln -s `pwd`/zsh/.zshrc ~/.zshrc
-ln -s `pwd`/ack/.ackrc ~/.ackrc
-ln -s `pwd`/tmux/.tmux.conf ~/.tmux.conf
-ln -s `pwd`/alacritty/.alacritty.yml ~/.alacritty.yml
-ln -s `pwd`/global_gitignore/.global_gitignore ~/.global_gitignore
+ln -sf `pwd`/zsh/.zshrc ~/.zshrc
+ln -sf `pwd`/ack/.ackrc ~/.ackrc
+ln -sf `pwd`/tmux/.tmux.conf ~/.tmux.conf
+ln -sf `pwd`/alacritty/.alacritty.yml ~/.alacritty.yml
+ln -sf `pwd`/global_gitignore/.global_gitignore ~/.global_gitignore
 
 git config --global core.excludesfile ~/.global_gitignore
+
+echo 'Installing brew dependencies'
+brew bundle
 
 echo 'Done c:'
